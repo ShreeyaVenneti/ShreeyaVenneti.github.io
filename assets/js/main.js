fcda3e43039da49a55b1e67b74f9dfc0719e2d83
ShreeyaVenneti <vennetishreeya@gmail.com>
2022-08-29 11:12:26 +0530

    Made modifications to the JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -248,3 +248,20 @@
 			._parallax();
 
 })(jQuery);
+
+// async function typeSentence(sentence, eleRef, delay = 100) {
+// 	const letters = sentence.split("");
+// 	let i = 0;
+// 	while(i < letters.length) {
+// 	  await waitForMs(delay);
+// 	  $(eleRef).append(letters[i]);
+// 	  i++
+// 	}
+// 	return;
+//   }
+  
+  
+//   function waitForMs(ms) {
+// 	return new Promise(resolve => setTimeout(resolve, ms))
+//   }
+//   typeSentence("Hi! I'm Shreeya Venneti", "#sentence")
